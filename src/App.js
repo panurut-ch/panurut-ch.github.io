fcd6252b2455f4f60398e81ae28e222754b31298
ARMY <panuc129@syner.postbox.in.th>
2021-03-15 10:57:53 +0700

    V1.2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React from "react";
-import Table from "./component/Table";
 import "./App.css";
-import Datepicker from "./component/Datepicker";
-import Autocompleter from "./component/Autocomplete";
 import "react-block-ui/style.css";
-import { Card, Container, Row, Col } from "react-bootstrap";
+import { Box } from '@material-ui/core';
+import Index from "./component/index";
 
 function App() {
   return (
     <div className="App">
-      <Card border="primary" style={{ width: "70%", margin: "5%" }}>
-        <Card.Body>
-          <Card.Title>
-            <h1>Hello</h1>
-          </Card.Title>
-          <Card.Text>
-            <Container style={{ border: "1px solid black" }}>
-              <Row>
-                <Col>
-                  <label className="label"> Date & Timepicker : </label>
-                  <Datepicker />
-                </Col>
-                <Col>
-                  <label className="label"> Autocomplete from API : </label>
-                  <Autocompleter />
-                </Col>
-              </Row>
-            </Container>
-            <hr />
-            <Table />
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <Box m={3}>
+          <Index />
+        </Box>
     </div>
   );
 }
--- src/component/Datepicker.js
+++ src/component/Datepicker.js
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import DateFnsUtils from "@date-io/date-fns";
 import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { Grid, FormControl } from "@material-ui/core";
+
+// import MomentUtils from "@date-io/moment";
+// import { MuiPickersUtilsProvider, DateTimePicker } from "material-ui-thai-datepickers";
+ 
+// import 'moment/locale/th';
 
 export default function Datepicker() {
-  const [selectedDate, handleDateChange] = useState(new Date());
+  const [selectedDate, handleDateChange] = useState(null);
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <DateTimePicker value={selectedDate} onChange={handleDateChange} />
-    </MuiPickersUtilsProvider>
+    <Grid container>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DateTimePicker 
+                label="Date & Time"
+                value={selectedDate} 
+                onChange={handleDateChange}
+              />
+            </MuiPickersUtilsProvider>  
+          </FormControl>
+        </Grid>
+      </Grid>
   );
 }
