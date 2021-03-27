52c7c31f730f6cdd67e6766f23f0a3e305587bb3
ARMY <panuc129@syner.postbox.in.th>
2021-03-28 03:01:22 +0700

    Navbar, Form

---
+++ src/component/Form.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
+import { TextField } from "@material-ui/core";
+import { Grid, Typography, useMediaQuery, useTheme } from "@material-ui/core";
+import Icon from "@material-ui/core/Icon";
+import SaveIcon from "@material-ui/icons/Save";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { Alert, AlertTitle } from "@material-ui/lab";
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h3">Form</Typography>
+        </Grid>
+        <Grid item md={4}>
+          <form onSubmit={this.handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item md={12}>
+                <div>
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="First Name"
+                    variant="outlined"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </Grid>
+              <Grid item md={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  startIcon={<SaveIcon />}
+                  type="submit"
+                >
+                  Save
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+      </Grid>
+    );
+  }
+}
---
+++ src/component/Navbar.js
@@ -0,0 +1,89 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-target-blank */
+import React, { Component } from "react";
+import Table from "./Table";
+import Datepicker from "./Datepicker";
+import Autocompleter from "./Autocomplete";
+import {
+  BrowserRouter,
+  Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  NavLink,
+} from "react-router-dom";
+import Index from "./index";
+import Form from "./Form";
+import Routing from "../routes/Routers";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="my-app">
+        <nav
+          className="navbar is-dark"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="container">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="https://panurut-ch.github.io/">
+                PANURUT
+              </a>
+            </div>
+            <div className="navbar-menu">
+              <div className="navbar-end">
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="is-active"
+                  className="navbar-item"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/form"
+                  activeClassName="is-active"
+                  className="navbar-item"
+                >
+                  Forms
+                </NavLink>
+                <NavLink
+                  to="/table"
+                  activeClassName="is-active"
+                  className="navbar-item"
+                >
+                  DataTable
+                </NavLink>
+                <NavLink
+                  to="/date"
+                  activeClassName="is-active"
+                  className="navbar-item"
+                >
+                  Datepicker
+                </NavLink>
+                <NavLink
+                  to="/auto"
+                  activeClassName="is-active"
+                  className="navbar-item"
+                >
+                  Autocomplete
+                </NavLink>
+                <a
+                  className="navbar-item"
+                  href="https://github.com/panurut-ch/panurut-ch.github.io"
+                  target="_blank"
+                >
+                  Github <i className="fab fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <br />
+        <Routing />
+      </div>
+    );
+  }
+}
